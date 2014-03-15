e458306ab8ad76aa20abb778759d240f56d20aa1
WPiotr <p.wyszkowski92@gmail.com>
2014-03-15 11:50:28 +0100

    Dodanie projektu

---
+++ BSK1/BSK1/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BSK1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            
+        }
+
+        private void Decryption1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Encrypt1(object sender, RoutedEventArgs e)
+        {
+            if (k.Text == "" || input.Text == "")
+            {
+                MessageBox.Show("Wprowadz wymagane pola!");
+            }
+            else 
+            {
+                result.Text = Utils.RailFenceEncrypt(int.Parse(k.Text), input.Text);
+            }
+        }
+
+        private void Encrypt2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Decryption2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Encrypt3(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Decryption3(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Encrypt4(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Decryption4(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Encrypt5(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Decryption5(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ BSK1/BSK1/Utils.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSK1
+{
+    public class Utils
+    {
+
+        public static String RailFenceEncrypt(int k, String text)
+        {
+            return RailFenceEncryptUp(k, text) + RailFenceEncryptMid(k, text) + RailFenceEncryptDown(k, text);
+        }
+
+        private static String RailFenceEncryptUp(int k, String text)
+        {
+            String result = "";
+            for (int i = 0; i * ((2 * k) - 2) < text.Length; i++)
+            {
+                result += text[i * ((2 * k) - 2)];
+            }
+            return result;
+        }
+
+        private static String RailFenceEncryptDown(int k, String text)
+        {
+            String result = "";
+            for (int i = 0; i * ((2 * k) - 2 + k - 1) < text.Length; i++)
+            {
+                result += text[i * ((2 * k) - 2 + k - 1)];
+            }
+            return result;
+        }
+
+        private static String RailFenceEncryptMid(int k, String text)
+        {
+            String result = "";
+            for (int i = 0; i < k - 2; i++)
+            {
+                for (int j = 0; j * ((2 * k) - 2) < text.Length; j++)
+                {
+                    int index = j * ((2 * k) - 2);
+                    if (text[index - 1] > 0)
+                    {
+                        result += text[index - 1];
+                    }
+                    if (text[index + 1] < text.Length)
+                    {
+                        result += text[index + 1];
+                    }
+                }
+            }
+            return result;
+        }
+
+
+     
+    }
+}
