4e70ce9947790c76d3d8e16a20cbd69a6ddbaa67
Chrystian Kisło <chrystian.kislo@gmail.com>
2014-03-18 23:01:10 +0100

    To jest już koniec

---
+++ BSK1/BSK1/VigenereEncrypt.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSK1
+{
+    public class VigenereEncrypt
+    {
+        public static String encrypt(String message, String key)
+        {
+            String encrypted = "";
+            for (int i = 0; i < message.Length; i++)
+            {
+                encrypted += (char)(((message[i] - 'A' + key[i % key.Length] - 'A') % 26) + 'A');
+            }
+            return encrypted;
+        }
+
+        public static String descrypt(String message, String key)
+        {
+            String descrypted = "";
+            for (int i = 0; i < message.Length; i++)
+            {
+                descrypted += (char)(modul((message[i] - 'A' - key[i % key.Length] + 'A') % 26,26) + 'A');
+            }
+            return descrypted;
+        }
+        private static int modul(int x, int n)
+        {
+            if(x>=0)
+                return x;
+            return n + x;
+        }
+
+    }
+}
---
+++ BSK1/BSK1Tests/VigenereEncryptTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BSK1;
+
+namespace BSK1Tests
+{
+    [TestClass]
+    public class VigenereEncryptTest
+    {
+        [TestMethod]
+        public void encryptTest()
+        {
+
+            String message = "CRYPTOGRAPHY";
+            String key = "BREAKBREAKBR";
+            String result_excepted = "DICPDPXVAZIP";
+
+            String result_actual = VigenereEncrypt.encrypt(message, key);
+
+            Assert.AreEqual(result_excepted, result_actual);
+        }
+        [TestMethod]
+        public void descryptTest()
+        {
+
+            String message = "DICPDPXVAZIP";
+            String key = "BREAKBREAKBR";
+            String result_excepted = "CRYPTOGRAPHY";
+
+            String result_actual = VigenereEncrypt.descrypt(message, key);
+
+            Assert.AreEqual(result_excepted, result_actual);
+        }
+    }
+}
