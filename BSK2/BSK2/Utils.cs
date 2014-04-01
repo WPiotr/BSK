1aa5b2d1c787fbe1536969db9e76d62dbcafc4db
WPiotr <p.wyszkowski92@gmail.com>
2014-04-01 20:06:23 +0200

    Dodanie drugiego projektu i jest interfejs

---
+++ BSK2/BSK2/Utils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSK2
+{
+    public class Utils
+    {
+        public static string Encrypt(String inputString, String key) {
+            return null;
+        }
+        public static string Decrypt(String inputString, String key) {
+            return null;
+        }
+
+
+    }
+}
