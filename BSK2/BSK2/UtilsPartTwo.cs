75749d007a545705b944c80c53f1a23116d3434b
WPiotr <p.wyszkowski92@gmail.com>
2014-04-08 18:49:43 +0200

    comit

--- BSK2/BSK2/UtilsPartTwo.cs
+++ BSK2/BSK2/UtilsPartTwo.cs
@@ -1,7 +1,47 @@
-﻿namespace BSK2
+﻿using System;
+using System.Collections;
+using System.IO;
+namespace BSK2
 {
     public partial class Utils
     {
 
     }
+
+    public class Message
+    {
+        private string msg;
+        private int[] ipTable = {58, 50, 42, 34, 26, 18, 10, 2,
+                                 60, 52, 44, 36, 28, 20, 12, 4,
+                                 62, 54, 46, 38, 30, 22, 14, 6,
+                                 64, 56, 48, 40, 32, 24, 16, 8,
+                                 57, 49, 41, 33, 25, 17, 9, 1,
+                                 59, 51, 43, 35, 27, 19, 11, 3,
+                                 61, 53, 45, 37, 29, 21, 13, 5,
+                                 63, 55, 47, 39, 31, 23, 15, 7};
+        public BitArray bitMsg;
+
+        public Message(string pathToFile) {
+            byte[] bytes = File.ReadAllBytes(pathToFile);
+            bitMsg = new BitArray(bytes);
+        }
+
+        public void initialPermutation()
+        {
+            
+            
+        }
+        public void splitting()
+        {
+        }
+        public void reverseConnecting()
+        {
+        }
+        public void finalPermutation()
+        {
+        }
+
+
+    }
+
 }
---
+++ BSK2/Tests/MsgTest.cs
@@ -0,0 +1,19 @@
+﻿namespace Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using BSK2;
+
+    [TestClass]
+    public class MsgTest
+    {
+        [TestMethod]
+        public void loadMsg()
+        {
+            string path ="test.bin";
+            Message msg = new Message(path);
+            CollectionAssert.AllItemsAreNotNull(msg.bitMsg);
+
+        }
+    }
+}
