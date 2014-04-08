6ebb88423da9b03554f9d443325b462eae2047ab
Chrystian Kisło <chrystian.kislo@gmail.com>
2014-04-08 15:41:06 +0200

    czytanie z hex do klucza, metody i jeden test do klucza

--- BSK2/BSK2/Utils.cs
+++ BSK2/BSK2/Utils.cs
@@ -3,18 +3,83 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace BSK2
 {
     public partial class Utils
     {
-        public static string Encrypt(String inputString, String key) {
+        public static string Encrypt(String inputString, String key)
+        {
             return null;
         }
-        public static string Decrypt(String inputString, String key) {
+        public static string Decrypt(String inputString, String key)
+        {
             return null;
         }
-
-
+    }
+    public class Key
+    {
+        string key;
+        int[] PC_1 =        {57,49,41,33,25,17,9,
+                            1,58,50,42,34,26,18,
+                            10,2,59,51,43,35,27,
+                            19,11,3,60,52,44,36,
+                            63,55,47,39,31,23,15,
+                            7,62,54,46,38,30,22,
+                            14,6,61,53,45,37,29,
+                            21,13,5,28,20,12,4};
+        public BitArray bit_key;
+        public BitArray key_plus;
+        public Key(string key){
+            this.key = key;
+            bit_key = new BitArray(key.Length * 4);
+            int j = 0;
+            foreach (char c in key)
+            {
+                byte[] byte_key = BitConverter.GetBytes(c);
+                if (Char.IsDigit(c))
+                {
+                    byte_key[0] -= (int)'0';
+                }
+                else
+                {
+                    byte_key[0] -= (int)'A';
+                    byte_key[0] += 10;
+                }
+                BitArray one_byte = new BitArray(byte_key);
+                for (int i = 3; i >= 0; i--,j++)
+                {
+                    bit_key.Set(j, one_byte[i]);
+                }
+            }
+        }
+        public void initialPermutation()
+        {
+        }
+        public void splitting()
+        {
+        }
+        public void shifts()
+        {
+        }
+        public void finalPermutation()
+        {
+        }
+    }
+    public class Message
+    {
+        public void initialPermutation()
+        {
+        }
+        public void splitting()
+        {
+        }
+        public void reverseConnecting()
+        {
+        }
+        public void finalPermutation()
+        {
+        }
     }
 }
---
+++ BSK2/Tests/KeyTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BSK2;
+
+namespace Tests
+{
+    [TestClass]
+    public class KeyTest
+    {
+        [TestMethod]
+        public void createKey()
+        {
+            string key = "133457799BBCDFF1";
+
+            BitArray excepted_key = new BitArray(64);
+            string given_key_table = "00010011 00110100 01010111 01111001 10011011 10111100 11011111 11110001";
+            for (int i = 0, j = 0; i < given_key_table.Length; i++)
+            {
+                if (given_key_table[i] == '0')
+                {
+                    excepted_key[j++] = false;
+                }
+                else if (given_key_table[i] == '1')
+                {
+                    excepted_key[j++] = true;
+                }
+            }
+            Key test_key = new Key(key);
+
+            CollectionAssert.AreEqual(excepted_key, test_key.bit_key, "Excepted:\n" + bitArrayToString(excepted_key) + " Actual\n" + bitArrayToString(test_key.bit_key));
+            /*
+            Key test_key = new Key(key);
+            Assert.AreEqual(excepted_key.Length, test_key.key_plus.Length, "Excepted: length = " + excepted_key.Length + "\n" + bitArrayToString(excepted_key) + " Actual:length = " + test_key.bit_key.Length + "\n" + bitArrayToString(test_key.bit_key));
+            for (int i = 0; i < excepted_key.Length;i++ )
+            {
+                Assert.AreEqual(excepted_key[i], test_key.key_plus[i], "Excepted:\n" + bitArrayToString(excepted_key) + " Actual\n" + bitArrayToString(test_key.bit_key));
+            }*/
+        }
+        private string bitArrayToString(BitArray array)
+        {
+            string bitArray = "";
+            for (int i = 0; i < array.Length; i++)
+            {
+                bool bit = array.Get(i);
+                bitArray += (bit ? '1' : '0');
+                if (i % 8 == 7 && i!=0)
+                {
+                    bitArray += ' ';
+                }
+            }
+            return bitArray;
+        }
+    }
+}
